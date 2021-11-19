8e82a02654f98aaa2341dbb2d34fa22c2689fec8
gokul gund <gokulgund15@gmail.com>
2021-11-19 12:36:09 +0530

    Added: Calculate Emp Wage using Switch case

---
+++ EmployeeWageUC4.java
@@ -0,0 +1,31 @@
+public class EmployeeWageUC4
+
+{
+ //constant
+    public static final int IS_FULL_TIME=1;
+    public static final int SALARY_PERHR=20;
+    public static final int IS_PART_TIME=2;
+
+
+
+   public static void main(String[] args)
+{
+    //Variable
+    int empCheck=(int)Math.floor((Math.random() * 10) % 3);
+    int Working_Hr=0;
+         switch(empCheck)
+              {
+             case IS_FULL_TIME:   Working_Hr=8;
+              break;
+             case IS_PART_TIME:
+                Working_Hr=4;
+             break;
+            default:
+              Working_Hr=0;
+             }
+         double Salary=Working_Hr*SALARY_PERHR;
+                  System.out.println("Emplyee Salary is="+Salary+  "  Doller");
+}
+
+
+}
