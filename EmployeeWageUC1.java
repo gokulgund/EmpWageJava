c61f3c3daa246c2f079346c9597a093444a784cc
gokul gund <gokulgund15@gmail.com>
2021-11-19 11:52:34 +0530

    Added: Emp is Present or Absent

---
+++ EmployeeWageUC1.java
@@ -0,0 +1,21 @@
+public class EmployeeWageUC1
+{
+   public static void main(String[] args)
+{
+
+    int IS_PRESENT=1;
+    double empCheck=Math.floor((Math.random() * 10) % 2);
+
+         if (empCheck == IS_PRESENT)
+              {
+
+                System.out.println("Employee is Present");
+              }
+        else
+              {
+                System.out.println("Employee is Absent");
+              }
+}
+
+
+}
