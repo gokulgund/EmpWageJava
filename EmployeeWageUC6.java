28f45bfe296a823d50ff1311599124c079a1782e
gokul gund <gokulgund15@gmail.com>
2021-11-19 14:59:36 +0530

    Added: Calculate Emp Working till max Hrs or MaxDay in month

---
+++ EmployeeWageUC6.java
@@ -0,0 +1,41 @@
+public class EmployeeWageUC6
+
+{
+ //constant
+    public static final int IS_FULL_TIME=1;
+    public static final int SALARY_PERHR=20;
+    public static final int IS_PART_TIME=2;
+    public static final int NO_OF_DAYS=20;
+    public static final int MAX_HR_IN_MONTH=100;
+
+   public static void main(String[] args)
+{
+    //Variable
+    int Working_Hr=0;
+     int TotalEmpHrs=0;
+    int TotalEmpWorkingDay=0;
+          while (TotalEmpHrs<=MAX_HR_IN_MONTH && TotalEmpWorkingDay<NO_OF_DAYS)
+          {
+            TotalEmpWorkingDay++;
+             int empCheck=(int)Math.floor((Math.random() * 10) % 3);
+
+            switch(empCheck)
+                 {
+                   case IS_FULL_TIME:   Working_Hr=8;
+                    break;
+                   case IS_PART_TIME:
+                             Working_Hr=4;
+                   break;
+                  default:
+                      Working_Hr=0;
+                 }
+              TotalEmpHrs +=Working_Hr;
+                    System.out.println("Day :  " + TotalEmpWorkingDay + "   Emp Hr : " +Working_Hr);
+         }
+         int totalEmpWage=TotalEmpHrs *SALARY_PERHR;
+
+                  System.out.println("Total Emp Wage : " +totalEmpWage);
+}
+
+
+}
